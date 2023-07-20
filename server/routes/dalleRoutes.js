e0ac777be45f78c9ee26b08e936519552ab8e963
MOHAMMAD SHOEB FAIZAN <shoebfaizan71@gmail.com>
2023-07-21 00:37:48 +0530

    Update dalleRoutes.js

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -16,6 +16,27 @@ router.route('/').get((req, res) => {
   res.status(200).json({ message: 'Hello from VisionCraftAI' });
 });
 
+// router.route('/').post(async (req, res) => {
+//   try {
+//     const { prompt } = req.body;
+
+//     const aiResponse = await openai.createImage({
+//       prompt,
+//       n: 1,
+//       size: '1024x1024',
+//       response_format: 'b64_json',
+//     });
+
+//     const image = aiResponse.data.data[0].b64_json;
+//     res.status(200).json({ photo: image });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+//   }
+// });
+
+// ...
+
 router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
@@ -34,5 +55,6 @@ router.route('/').post(async (req, res) => {
     res.status(500).send(error?.response.data.error.message || 'Something went wrong');
   }
 });
+
 
 export default router;
